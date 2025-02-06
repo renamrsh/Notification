c449c9124556dbc9d3a22f1f66646222f2530f8b
renamrsh <irenamarsh@gmail.com>
2025-02-06 11:32:55 +0100

    changes

--- app/src/main/java/com/example/notification/NotificationHelper.java
+++ app/src/main/java/com/example/notification/NotificationHelper.java
@@ -12,40 +12,34 @@ import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 
-import android.content.Context;
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
 import androidx.core.content.ContextCompat;
-
-import kotlinx.coroutines.channels.Channel;
 
 public class NotificationHelper {
     private static final String CHANNEL_ID = "default_channel";
     private static final String CHANNEL_NAME = "Kanał Powiadomień";
     private static final int NOTIFICATION_ID = 1;
+    // kanal ma importance wieksza niz same powiadomienia
 
-    public static void sendNotification(AppCompatActivity activity, Context context, String title, String message, int styleType, Integer largeIconResID, Integer sound){
+    public static void sendNotification(int NOTIFICATION_ID, int Priority, AppCompatActivity activity, String title, String message, int styleType, Integer largeIconResID, Integer sound){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
             if(ContextCompat.checkSelfPermission(activity, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED){
                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 100);
                 return;
             }
         }
-
         NotificationManager notificationManager = (NotificationManager) activity.getSystemService(Context.NOTIFICATION_SERVICE);
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            //priorytet kanalu powiadomien, poniewaz powiadomiana nie dzialaja
+            //trzeba stworzyc kilka kanalow, i wybierac w zaleznosci od potrzebnej importance
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW);
             notificationManager.createNotificationChannel(channel);
-
             if(sound!=null){
-                Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+context.getApplicationContext()
+                Uri soundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+"://"+activity.getApplicationContext()
                         .getPackageName()+ "/"+sound);
                 channel.setSound(soundUri, null);
-
             }
         }
 
@@ -53,21 +47,21 @@ public class NotificationHelper {
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle(title) //  \/ make whole text (long, more the two rows) display in message window
                 .setContentText(message)
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setPriority(Priority)
                 //.setContentIntent(pendingIntent) // po kliknieciu prowadzi do aplikacji
                 .setAutoCancel(true);
 
-    if(largeIconResID!=null){
-        Bitmap largeIcon = BitmapFactory.decodeResource(context.getResources(), largeIconResID);
-        builder.setLargeIcon(largeIcon);
-    }
+        if(largeIconResID!=null){
+            Bitmap largeIcon = BitmapFactory.decodeResource(activity.getResources(), largeIconResID);
+            builder.setLargeIcon(largeIcon);
+        }
 
         switch(styleType){
             case 1:
                 builder.setStyle(new NotificationCompat.BigTextStyle().bigText(message));
                 break;
             case 2:
-                Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_launcher_foreground);
+                Bitmap bitmap = BitmapFactory.decodeResource(activity.getResources(), largeIconResID);
                 builder.setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).setBigContentTitle(title));
                 break;
             case 3:
@@ -79,4 +73,5 @@ public class NotificationHelper {
         }
         notificationManager.notify(NOTIFICATION_ID, builder.build());
     }
+
 }
